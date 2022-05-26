6c213480898d91f3a9e05fd2e962f2354c4df8a0
mishkanchik <csprince666@gmail.com>
2022-05-26 18:59:22 +0300

    синтексис компоненту

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom/client';
 
-let h1 = <h1>hello world</h1>
+// const el = (
+//     <div>
+//         <div>
+//             <ul>
+//                 <li>list item 1 </li>
+//                 <li>list item 1 </li>
+//                 <li>list item 1 </li>
+//                 <li>list item 1 </li>
+//                 <li>list item 1 </li>
+//             </ul>
+//         </div>
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio expedita totam iusto! Nobis ea esse quae veniam magnam omnis numquam, id eligendi harum! Harum consectetur repellendus eligendi nihil quibusdam minima.</p>
+//     </div>
+// )
+
+function App () {
+    return(
+        <h1> hello App.js</h1>
+    )
+
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(h1)
+root.render(<App/>)
 
