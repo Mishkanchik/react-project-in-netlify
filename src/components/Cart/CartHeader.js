0ec6602310c757974bdec7d394c9c134c596671b
mishkanchik <csprince666@gmail.com>
2022-05-31 19:20:40 +0300

    commit module

---
+++ src/components/Cart/CartHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './CartHeader.module.css'
+export const CartHeader = () => {
+    return (
+        <div className={styles.сartHeader}>
+            <div>0</div>
+            <div>0$</div>
+        </div>
+    )
+}
